56dd4e479fcb57a8e304c71a5f2b764160992351
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2015-12-12 14:52:10 -0800

    adding sample projects, not wired up yet

---
+++ MultiProjTemplate/ProjectTemplates/Web/SampleMultiProj/ConsoleApplication/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine(string.Format("Name: {0}",SharedLibrary.SharedCLass.Name));
+        }
+    }
+}
